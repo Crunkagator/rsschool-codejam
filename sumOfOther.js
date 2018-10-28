04ba24cc896688f367a9cb0d6b8fb7a83ed8b2a4
Crunkagator <briantheblack@gmail.com>
2018-10-28 20:55:44 +0300

    feat: modified tests via mocha

--- sumOfOther.js
+++ sumOfOther.js
@@ -1,3 +1,3 @@
 module.exports = function sumOfOther(arr) {
-  return arr.map(x => arr.reduce((a, b) => a + b) - x);
+  return arr.length > 0 ? arr.map(x => arr.reduce((a, b) => a + b) - x) : new Error('No items');
 };
